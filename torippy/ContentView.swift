1b7ae9f64b38b0704b294e1dd94512af7ece4c0d
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-24 18:57:31 +0900

    Initial Commit

---
+++ torippy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  torippy
+//
+//  Created by 櫻井絵理香 on 2024/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ torippy/torippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  torippyApp.swift
+//  torippy
+//
+//  Created by 櫻井絵理香 on 2024/03/24.
+//
+
+import SwiftUI
+
+@main
+struct torippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
